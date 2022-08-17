33a7ce4f2409a294c5e94a91a6f038d71b382b9c
yujinkim11 <wowyj26@naver.com>
2022-08-17 12:57:04 +0900

    action, payload

--- src/component/ContactForm.js
+++ src/component/ContactForm.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
 
 export const ContactForm = () => {
   const [name, setName] = useState();
   const [phoneNum, setPhoneNum] = useState();
+  const dispatch = useDispatch();
 
   const getName = (event) => {
     console.log(event.target.value);
@@ -15,9 +17,14 @@ export const ContactForm = () => {
     setPhoneNum(event.target.value);
   };
 
+  const addContact = (event) => {
+    event.preventDefault();
+    dispatch({ type: "ADD_CONTACT", payload: { name, phoneNum } });
+  };
+
   return (
     <div>
-      <Form>
+      <Form onSubmit={addContact}>
         <Form.Group className="mb-3" controlId="formName">
           <Form.Label>이름</Form.Label>
           <Form.Control
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,5 +1,21 @@
-let initialState = {};
+let initialState = {
+  contactList: [],
+};
 
-function reducer(state = initialState, action) {}
+function reducer(state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case "ADD_CONTACT":
+      return {
+        ...state,
+        contactList: [
+          ...state.contactList,
+          { name: payload.name, phoneNum: payload.phoneNum },
+        ],
+      };
+    default:
+      return { ...state };
+  }
+}
 
 export default reducer;
